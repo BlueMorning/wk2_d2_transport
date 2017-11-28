cc921cb2f9cb1aa35a6a3dd53bbd0c9e84595321
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2017-11-28 14:05:08 +0000

    Test 'test_pick_up_passenger' passed

--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -1,11 +1,13 @@
 require ("MiniTest/autorun")
 require ("MiniTest/rg")
 require_relative("../bus")
+require_relative("../person")
 
 class TestBus < MiniTest::Test
 
   def setup
     @bus = Bus.new(22, "Ocean Terminal")
+    @passenger1 = Person.new("Annie Smith", 80)
   end
 
   def test_get_route_number
@@ -28,4 +30,8 @@ class TestBus < MiniTest::Test
     assert_equal(0, @bus.passengers_number)
   end
 
+  def test_pick_up_passenger
+    @bus.pick_up(@passenger1)
+    assert_equal(1, @bus.passengers_number)
+  end
 end
